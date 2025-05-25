8daf35ca34b484bcbe47336e69f7a5cdfe0d27e5
Park JaeWook <>
2025-05-25 15:33:20 +0900

    init

--- main.ts
+++ main.ts
@@ -1,134 +1,67 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
-
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+import {  Editor, Plugin, requestUrl, Notice } from 'obsidian';
+import * as dotenv from 'dotenv';
 
 export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
 	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		// 이미지 붙여넣기 이벤트 감지
+		this.registerEvent(
+			this.app.workspace.on('editor-paste',this.handlePaste.bind(this))
+		)
 	}
 
 	onunload() {
-
 	}
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	async handlePaste(evt: ClipboardEvent, editor: Editor){
+		const items = evt.clipboardData?.items;
+		if(!items) return;
+
+		for( let i = 0; i < items.length; i++ ){
+			const item = items[i];
+			if(item.type.indexOf('image') !== -1){
+				evt.preventDefault();
+
+				const file = item.getAsFile();
+				if(file){
+					try {
+						const imageUrl = await this.uploadToCloudFlare(file);
+						editor.replaceSelection(`![Image](${imageUrl})`);
+					} catch (error) {
+						// 에러 처리
+						new Notice(`이미지 업로드 실패: ${error.message}`, 5000);
+						console.error('Image upload failed:', error);
+					}
+				}
+			}
+		}
 	}
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+	async uploadToCloudFlare(file:File){
+		const token = process.env["CLOUD_FLARE_TOKEN "];
+		const id = process.env["CLOUD_FLARE_ID "];
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
+		if(!token || !id){
+			console.error('환경변수 설정 오류');
+			return;
+		}
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+		const formData = new FormData();
+		formData.append('file', file);
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+		const response = await requestUrl(
+			{
+				url:`https://api.cloudflare.com/client/v4/accounts/${id}/images/v2/direct_upload`,
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+					Authorization: `Bearer ${token}`,
+				},
+			}
+		);
+		const result = JSON.parse(response.text).result;
+		await fetch(result.uploadURL,{body:formData, method:'post'})
+		return `https://imagedelivery.net/0ls7rUWRjBE8C2UFoz_eSw/${result.id}/post`
 	}
 }
